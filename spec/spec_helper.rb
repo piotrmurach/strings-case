60c1c2194424016824963bacd6d66338db9c4b83
Piotr Murach <pmurach@gmail.com>
2019-11-09 22:31:46 +0000

    Add check for modern ruby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,15 @@ end
 require "bundler/setup"
 require "strings/case"
 
+module Helpers
+  def modern_ruby?
+    Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.4")
+  end
+end
+
 RSpec.configure do |config|
+  config.extend Helpers
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
