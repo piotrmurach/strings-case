c365c4e0323956dd18df6eb4f9f868d6e311f062
Piotr Murach <pmurach@gmail.com>
2019-11-23 14:44:26 +0000

    Change to fix keyword argument warnings in Ruby 2.7

--- lib/strings/case/extensions.rb
+++ lib/strings/case/extensions.rb
@@ -4,45 +4,45 @@ module Strings
   module Case
     module Extensions
       refine String do
-        def camelcase(*args)
-          Strings::Case.camelcase(self, *args)
+        def camelcase(*args, **options)
+          Strings::Case.camelcase(self, *args, **options)
         end
         alias lower_camelcase camelcase
 
-        def constcase(*args)
-          Strings::Case.constcase(self, *args)
+        def constcase(*args, **options)
+          Strings::Case.constcase(self, *args, **options)
         end
         alias constantcase constcase
 
-        def headercase(*args)
-          Strings::Case.headercase(self, *args)
+        def headercase(*args, **options)
+          Strings::Case.headercase(self, *args, **options)
         end
 
-        def kebabcase(*args)
-          Strings::Case.kebabcase(self, *args)
+        def kebabcase(*args, **options)
+          Strings::Case.kebabcase(self, *args, **options)
         end
         alias dashcase kebabcase
 
-        def pascalcase(*args)
-          Strings::Case.pascalcase(self, *args)
+        def pascalcase(*args, **options)
+          Strings::Case.pascalcase(self, *args, **options)
         end
         alias upper_camelcase pascalcase
 
-        def pathcase(*args)
-          Strings::Case.pathcase(self, *args)
+        def pathcase(*args, **options)
+          Strings::Case.pathcase(self, *args, **options)
         end
 
-        def sentencecase(*args)
-          Strings::Case.sentencecase(self, *args)
+        def sentencecase(*args, **options)
+          Strings::Case.sentencecase(self, *args, **options)
         end
 
-        def snakecase(*args)
-          Strings::Case.snakecase(self, *args)
+        def snakecase(*args, **options)
+          Strings::Case.snakecase(self, *args, **options)
         end
         alias underscore snakecase
 
-        def titlecase(*args)
-          Strings::Case.titlecase(self, *args)
+        def titlecase(*args, **options)
+          Strings::Case.titlecase(self, *args, **options)
         end
       end
     end # Extensions
