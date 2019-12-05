bcdeed78d28694cc6e13405696304e387667572f
Piotr Murach <pmurach@gmail.com>
2019-12-05 21:08:41 +0000

    Add baseline parsecase performance tests

---
+++ spec/perf/parsecase_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rspec-benchmark"
+require "active_support"
+
+RSpec.describe Strings::Case do
+  include RSpec::Benchmark::Matchers
+
+  it "changes case" do
+    expect {
+      Strings::Case.snakecase("fooBarBaz")
+    }.to perform_slower_than {
+      ActiveSupport::Inflector.underscore("fooBarBaz")
+    }.at_most(4).times
+  end
+
+  it "allocates no more than 100 objects" do
+    expect {
+      Strings::Case.snakecase("fooBarBaz")
+    }.to perform_allocation(42)
+  end
+end
